841f79adc263aa9553375420448a56e162e3bfa3
unknown <anthonyaadamson@gmail.com>
2023-08-31 19:02:04 -0400

    fetched the data for the giphy api in the main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,22 @@ var searchBtn = document.querySelector('#searchBtn');
 
 function searchInput(event) {
     event.preventDefault();
-    var search = searchEl.value;
-    console.log(search);
-  }
+    var searchResult = searchEl.value;
+    console.log(searchResult);
+  };
   searchBtn.addEventListener("click", searchInput);
 
-  function searchBtn(event) {
-    event.preventDefault();
-    var search = searchBtn.value;
-    console.log(search);
-  }
+function getGiphyApi() {
+  var searchResult = searchResult
+  var GIPHY_API_KEY = 'XqV9B07FkKPoAffhSG7aPooSHyN7zfCo'
+  var giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + GIPHY_API_KEY + '&q=' + searchResult + '&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
   
+  fetch(giphyUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  };
+  searchBtn.addEventListener("click", getGiphyApi);
