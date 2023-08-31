144fc5c1416ededbde6978d05a1dd2a6db7ff1ce
unknown <anthonyaadamson@gmail.com>
2023-08-30 20:15:38 -0400

    add giphy js file with api key

---
+++ assets/js/giphy.js
@@ -0,0 +1,4 @@
+var API_KEY = 'XqV9B07FkKPoAffhSG7aPooSHyN7zfCo'
+
+var url = 'https://api.giphy.com/v1/gifs/search?api_key=' + API_KEY + '&q=' + searchResult + '&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
+
